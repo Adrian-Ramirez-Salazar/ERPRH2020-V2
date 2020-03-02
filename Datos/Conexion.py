09d3d04a286e486d78e9a9a9efc34fab352cc60a
Adrian-Ramirez-Salazar <60801757+Adrian-Ramirez-Salazar@users.noreply.github.com>
2020-03-01 21:33:21 -0600

    v2

--- Datos/Conexion.py
+++ Datos/Conexion.py
@@ -3,7 +3,7 @@ class Conexion:
     db=None
 
     def __init__(self):
-        self.db=pyodbc.connect(Driver="{SQL Server};", Server="DESKTOP-8SKO2G9\SQLEXPRESS;", db='ERP2020;', Trusted_Connection='yes;')
+        self.db=pyodbc.connect(Driver="{SQL Server};", Server="localhost;", db='ERP2020;', Trusted_Connection='yes;')
 
     def getDB(self):
         return self.db
--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request, session, flash
+from flask import Flask, render_template, request, session, flash, url_for
 from flask_sqlalchemy import SQLAlchemy
 import pyodbc
+from werkzeug.utils import redirect
+
 from Datos.usuarioDAO import UsuarioDAO
 
 
@@ -25,6 +27,7 @@ def index():
 
 
 
+
 #Direcciona a la pagina principal del sistema
 @app.route('/login', methods=['GET','POST'])
 def login():
@@ -42,6 +45,31 @@ def login():
     except:
         flash('Datos incorrectos')
     return render_template('Comunes/principal.html')
+
+
+
+
+
+#Muestra el usuario que ingreso al sistema (USUARIOS DIRECTOS DE SQL)
+@app.route('/Usuario', methods=['POST'])
+def Usuario():
+    try:
+        server = 'localhost'
+        database = 'ERP2020'
+        username = session['usr']
+        password = session['pass']
+        cnxn = pyodbc.connect(
+            'DRIVER={ODBC Driver 17 for SQL Server}; SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
+        cursor = cnxn.cursor()
+        usr = session['usr']
+        interface = "Principal"
+        est = ("", "", "")
+        session['bandera'] = 1
+    except:
+        flash('Sesion terminada')
+    return render_template('Comunes/principal.html', usuario=usr, interfaz=interface, estado=est)
+
+
 
 
 
@@ -72,9 +100,7 @@ def Ciudades():
 @app.route('/Estado')
 def Estado():
     return render_template('Estado/ConsultaGeneralEstado.html')
-@app.route('/Puestos')
-def Puestos();
-    return render_template()''
+
 
 
 
@@ -112,10 +138,10 @@ def insertarDeduccion():
 #Lista General de Deducciones
 @app.route('/listarDeducciones')
 def listarDeducciones():
-    cursor = conn.cursor()
-    cursor.execute('Select idDeduccion, nombre, descripcion, porcentaje from RH.Deducciones')
-    data = cursor.fetchall()
-    return render_template('Deducciones/Deducciones.html', deducciones=data)
+        cursor = conn.cursor()
+        cursor.execute('Select * from RH.Deducciones;')
+        data = cursor.fetchall()
+        return render_template('Deducciones/Deducciones.html', deducciones=data)
 
 
 
