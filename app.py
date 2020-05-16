32271b96b48a15719546bc975952b73202426c42
Adrian-Ramirez-Salazar <60801757+Adrian-Ramirez-Salazar@users.noreply.github.com>
2020-05-15 21:28:21 -0500

    Este no cuenta :v

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def get_users(offset=0, per_page=10):
 app.secret_key=b'yangars'
 
 conn = pyodbc.connect('Driver={SQL Server};'
-                      'Server=WORKSTATION-PC\MSSQLSERVER2017;'
+                      'Server=DESKTOP-8SKO2G9\SQLEXPRESS;'
                       'Database=ERP2020;'
                       'Trusted_Connection=yes;')
 cursor = conn.cursor()
